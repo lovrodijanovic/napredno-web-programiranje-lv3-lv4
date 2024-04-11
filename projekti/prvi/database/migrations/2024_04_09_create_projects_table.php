2f66864a9fddc0a104ab877123374a896d2ac57c
lovrodijanovic <lovrodijanovic98@gmail.com>
2024-04-11 23:59:57 +0200

    Initial commit

---
+++ projekti/prvi/database/migrations/2024_04_09_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('price');
+            $table->string('jobs_finished');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
